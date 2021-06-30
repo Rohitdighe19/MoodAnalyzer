4b6dbfab91d164167c8dfec1dde893fe0ec1ecc6
rohitdighe19 <rohitdighe47@gmail.com>
2021-06-30 21:54:46 +0530

    Added: Solution to given NULL mood message should retrun HAPPY

--- main/java/analyzer/moodAnalyzer/MoodAnalyzer.java
+++ main/java/analyzer/moodAnalyzer/MoodAnalyzer.java
@@ -2,22 +2,28 @@ package analyzer.moodAnalyzer;
 
 
 public class MoodAnalyzer 
-{
-	private static String message;
-	
+{	
 	public static String MoodAnalyzer() {
-		message="I am in Happy Mood";
-		if(message.contains("Sad"))
-			return "SAD";
-		else
+		String message=null;
+		try{
+			if(message.contains("Sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		}catch(NullPointerException e){
 			return "HAPPY";
+		}
 	}
 	
 	public static String MoodAnalyzer(String message) {
-		if(message.contains("Sad"))
-			return "SAD";
-		else
+		try{
+			if(message.contains("Sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		}catch(NullPointerException e){
 			return "HAPPY";
+		}
 	}
 
 	
